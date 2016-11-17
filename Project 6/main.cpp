af139e62e0bb546ed9db042ef4c04e18408cf8b7
Alex Berthon <Alexjberthon@hotmail.com>
2016-11-16 20:30:57 -0800

    Initial Commit

---
+++ Project 6/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Project 6
+//
+//  Created by Alex Berthon on 11/16/16.
+//  Copyright © 2016 Alex Berthon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    
+    //randomly generate 10,000 integers between 1-100
+    //store them in a container.
+    //print all 10,000 to the console on one screen, sorted, smallest first largest last,
+    //print the sum of all characters
+    //pause console, ask for user input to continue
+    //print average of all int values
+    //print 1 - 100 and the frequency of that number next to it.
+    
+    
+    //efficient
+    //sorted smallest to largest
+    //duplicate elements
+    
+    //simple array? sort via quicksort
+}
